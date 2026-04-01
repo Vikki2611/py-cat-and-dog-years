30312bd7bc789551e6a870f10425dc2cfc9049a7
Victoria <nopasaran85@gmail.com>
2026-04-01 12:44:27 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_zero_age() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_less_than_15() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_exactly_15() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_between_15_and_24() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_exactly_24() -> None:
+    assert get_human_age(24, 24) == [2, 2]
